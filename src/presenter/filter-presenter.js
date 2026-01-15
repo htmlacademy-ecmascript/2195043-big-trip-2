31c0ab07814116984c57bf7153bf7845fdf1a868
teplostanski <igor@teplostanski.me>
2026-01-16 01:23:47 +0300

    Добавляет данные в фильтрацию

--- src/presenter/filter-presenter.js
+++ src/presenter/filter-presenter.js
@@ -1,14 +1,76 @@
 import FilterView from '../view/filter-view';
 import { render } from '../utils/render.js';
+import { FilterType, getFilterAvailability } from '../utils/index.js';
 
 export default class FilterPresenter {
-  filterComponent = new FilterView();
+  #filterComponent = null;
+  #container = null;
+  #pointsModel = null;
+  #currentFilter = FilterType.EVERYTHING;
 
-  constructor({ container }) {
-    this.container = container;
+  constructor({ container, pointsModel }) {
+    this.#container = container;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    render(this.filterComponent, this.container);
+    this.#renderFilters();
+    this.#setFilterChangeHandler();
+  }
+
+  #renderFilters() {
+    if (this.#filterComponent) {
+      this.#filterComponent.removeElement();
+    }
+
+    const points = this.#pointsModel.getPoints();
+    const filterAvailability = getFilterAvailability(points);
+    
+    this.#filterComponent = new FilterView(filterAvailability, this.#currentFilter);
+    render(this.#filterComponent, this.#container);
+  }
+
+  #setFilterChangeHandler() {
+    const element = this.#filterComponent.element;
+    if (!element) {
+      return;
+    }
+
+    const radioButtons = element.querySelectorAll('input[type="radio"][name="trip-filter"]');
+    radioButtons.forEach((radio) => {
+      radio.removeEventListener('change', this.#filterChangeHandler);
+      radio.addEventListener('change', this.#filterChangeHandler);
+    });
+
+    const form = element.querySelector('.trip-filters');
+    if (form) {
+      form.removeEventListener('change', this.#filterChangeHandler);
+      form.addEventListener('change', this.#filterChangeHandler);
+      form.removeEventListener('submit', this.#formSubmitHandler);
+      form.addEventListener('submit', this.#formSubmitHandler);
+    }
+  }
+
+  #filterChangeHandler = (evt) => {
+    const target = evt.target;
+    if (target && target.type === 'radio' && target.name === 'trip-filter' && !target.disabled) {
+      const newFilter = target.value;
+      if (newFilter !== this.#currentFilter) {
+        this.#currentFilter = newFilter;
+        if (this.onFilterChange) {
+          this.onFilterChange(this.#currentFilter);
+        }
+      }
+    }
+  };
+
+  #formSubmitHandler = (evt) => {
+    evt.preventDefault();
+  };
+
+  setFilter(filterType) {
+    this.#currentFilter = filterType;
+    this.#renderFilters();
+    this.#setFilterChangeHandler();
   }
 }
--- src/view/filter-view/filter-view.js
+++ src/view/filter-view/filter-view.js
@@ -1,9 +1,16 @@
 import AbstractView from '../../framework/view/abstract-view.js';
 import { createFilterTemplate } from './filter-template.js';
+import { FilterType } from '../../utils/index.js';
 
 export default class FilterView extends AbstractView {
+  constructor(filterAvailability = {}, currentFilter = FilterType.EVERYTHING) {
+    super();
+    this.filterAvailability = filterAvailability;
+    this.currentFilter = currentFilter;
+  }
+
   get template() {
-    return createFilterTemplate();
+    return createFilterTemplate(this.filterAvailability, this.currentFilter);
   }
 }
 
