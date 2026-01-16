35dff769b1a529c78e76f46266c29bdcd8c3778a
teplostanski <igor@teplostanski.me>
2026-01-16 16:13:17 +0300

    Добавляет условный рендеринг для фильтрации

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,8 +1,10 @@
 import SortView from '../view/sort-view';
 import ListView from '../view/list-view';
+import EmptyListMessageView from '../view/empty-list-message-view';
 import FormEditView from '../view/form-edit-view';
 import PointView from '../view/point-view';
-import { render, RenderPosition } from '../utils/render.js';
+import { render } from '../utils/render.js';
+import { FilterType, filterPoints } from '../utils/index.js';
 
 export default class BoardPresenter {
   sortComponent = new SortView();
@@ -12,6 +14,7 @@ export default class BoardPresenter {
   #offersModel = null;
   #pointComponents = new Map();
   #currentEditingPointId = null;
+  #currentFilter = FilterType.EVERYTHING;
 
   constructor({ container, pointsModel, destinationsModel, offersModel }) {
     this.container = container;
@@ -21,10 +24,45 @@ export default class BoardPresenter {
   }
 
   #renderBoard() {
+    const allPoints = this.#pointsModel.getPoints();
+    const filteredPoints = filterPoints(allPoints, this.#currentFilter);
+
+    if (filteredPoints.length === 0) {
+      this.#renderEmptyList();
+    } else {
+      this.#renderPointsList(filteredPoints);
+    }
+  }
+
+  #renderEmptyList() {
+    this.#pointComponents.clear();
+    this.container.innerHTML = '';
+    const emptyListComponent = new EmptyListMessageView(this.#currentFilter);
+    render(emptyListComponent, this.container);
+  }
+
+  setFilter(filterType) {
+    this.#currentFilter = filterType;
+    this.#renderBoard();
+  }
+
+  #renderPointsList(points) {
+    this.#pointComponents.clear();
+    
+    if (this.sortComponent.element && this.container.contains(this.sortComponent.element)) {
+      this.sortComponent.removeElement();
+    }
+    if (this.listComponent.element && this.container.contains(this.listComponent.element)) {
+      this.listComponent.removeElement();
+    }
+    
+    this.container.innerHTML = '';
+    
+    this.sortComponent = new SortView();
+    this.listComponent = new ListView();
+    
     render(this.sortComponent, this.container);
     render(this.listComponent, this.container);
-
-    const points = this.#pointsModel.getPoints();
 
     this.#renderPoints(points);
   }
@@ -55,10 +93,6 @@ export default class BoardPresenter {
 
   #replacePointToForm(point) {
     const destination = this.#destinationsModel.getDestinationById(point.destination);
-    const availableOffers = this.#offersModel.getOffersByType(point.type);
-    const selectedOffers = availableOffers.filter((offer) =>
-      point.offers.includes(offer.id)
-    );
 
     const formComponent = new FormEditView(
       point,
