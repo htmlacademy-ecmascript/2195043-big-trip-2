94a1e4b4fb79ba3f91055726c1bbeded45030044
teplostanski <igor@teplostanski.me>
2026-01-19 16:28:17 +0300

    Исправляет замечание наставника

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,19 +1,18 @@
 import SortView from '../view/sort-view';
 import ListView from '../view/list-view';
 import EmptyListMessageView from '../view/empty-list-message-view';
-import FormEditView from '../view/form-edit-view';
-import PointView from '../view/point-view';
+import PointItemContainerView from '../view/point-item-container-view';
+import PointPresenter from './point-presenter.js';
 import { render } from '../utils/render.js';
 import { FilterType, filterPoints } from '../utils/index.js';
 
 export default class BoardPresenter {
-  sortComponent = new SortView();
-  listComponent = new ListView();
+  #sortComponent = null;
+  #listComponent = null;
   #pointsModel = null;
   #destinationsModel = null;
   #offersModel = null;
-  #pointComponents = new Map();
-  #currentEditingPointId = null;
+  #pointPresenters = [];
   #currentFilter = FilterType.EVERYTHING;
 
   constructor({ container, pointsModel, destinationsModel, offersModel }) {
@@ -35,8 +34,9 @@ export default class BoardPresenter {
   }
 
   #renderEmptyList() {
-    this.#pointComponents.clear();
-    this.container.innerHTML = '';
+    this.#clearPoints();
+    this.#clearComponents();
+    
     const emptyListComponent = new EmptyListMessageView(this.#currentFilter);
     render(emptyListComponent, this.container);
   }
@@ -47,104 +47,77 @@ export default class BoardPresenter {
   }
 
   #renderPointsList(points) {
-    this.#pointComponents.clear();
+    this.#clearPoints();
+    this.#clearComponents();
     
-    if (this.sortComponent.element && this.container.contains(this.sortComponent.element)) {
-      this.sortComponent.removeElement();
-    }
-    if (this.listComponent.element && this.container.contains(this.listComponent.element)) {
-      this.listComponent.removeElement();
-    }
+    this.#sortComponent = new SortView();
+    this.#listComponent = new ListView();
     
-    this.container.innerHTML = '';
-    
-    this.sortComponent = new SortView();
-    this.listComponent = new ListView();
-    
-    render(this.sortComponent, this.container);
-    render(this.listComponent, this.container);
+    render(this.#sortComponent, this.container);
+    render(this.#listComponent, this.container);
 
     this.#renderPoints(points);
   }
 
+  #clearPoints() {
+    this.#pointPresenters.forEach((presenter) => {
+      presenter.destroy();
+    });
+    this.#pointPresenters = [];
+  }
+
+  #clearComponents() {
+    if (this.#sortComponent) {
+      this.#sortComponent.removeElement();
+      this.#sortComponent = null;
+    }
+    if (this.#listComponent) {
+      this.#listComponent.removeElement();
+      this.#listComponent = null;
+    }
+    
+    while (this.container.firstChild) {
+      this.container.firstChild.remove();
+    }
+  }
+
+  #getPointData(point) {
+    const destination = this.#destinationsModel.getDestinationById(point.destination);
+    const availableOffers = this.#offersModel.getOffersByType(point.type);
+    const selectedOffers = availableOffers.filter((offer) =>
+      point.offers.includes(offer.id)
+    );
+
+    return { destination, availableOffers, selectedOffers };
+  }
+
   #renderPoints(points) {
     points.forEach((point) => {
-      const destination = this.#destinationsModel.getDestinationById(point.destination);
-      const availableOffers = this.#offersModel.getOffersByType(point.type);
-      const selectedOffers = availableOffers.filter((offer) =>
-        point.offers.includes(offer.id)
-      );
+      const { destination, selectedOffers } = this.#getPointData(point);
 
-      const pointComponent = new PointView(
+      const pointItemContainerView = new PointItemContainerView();
+      render(pointItemContainerView, this.#listComponent.element);
+
+      const pointPresenter = new PointPresenter({
+        container: pointItemContainerView.element,
         point,
         destination,
         selectedOffers,
-        () => this.#replacePointToForm(point)
-      );
+        destinationsModel: this.#destinationsModel,
+        offersModel: this.#offersModel
+      });
 
-      this.#pointComponents.set(point.id, { pointComponent, formComponent: null });
-
-      render(
-        pointComponent,
-        this.listComponent.element
-      );
+      this.#pointPresenters.push(pointPresenter);
+      pointPresenter.init();
     });
   }
 
-  #replacePointToForm(point) {
-    const destination = this.#destinationsModel.getDestinationById(point.destination);
-
-    const formComponent = new FormEditView(
-      point,
-      destination,
-      {
-        getOffersByType: (type) => this.#offersModel.getOffersByType(type),
-        getDestinations: () => this.#destinationsModel.getDestinations()
-      },
-      () => this.#replaceFormToPoint(point.id),
-      () => this.#replaceFormToPoint(point.id)
-    );
-
-    const components = this.#pointComponents.get(point.id);
-    const pointElement = components.pointComponent.element;
-    const formElement = formComponent.element;
-
-    pointElement.replaceWith(formElement);
-
-    components.formComponent = formComponent;
-    this.#currentEditingPointId = point.id;
-
-    document.addEventListener('keydown', this.#escKeyDownHandler);
-  }
-
-  #replaceFormToPoint(pointId) {
-    const components = this.#pointComponents.get(pointId);
-    if (!components || !components.formComponent) {
-      return;
-    }
-
-    const formElement = components.formComponent.element;
-    const pointElement = components.pointComponent.element;
-
-    formElement.replaceWith(pointElement);
-
-    components.formComponent.removeElement();
-    components.formComponent = null;
-    this.#currentEditingPointId = null;
-
-    document.removeEventListener('keydown', this.#escKeyDownHandler);
-  }
-
-  #escKeyDownHandler = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      if (this.#currentEditingPointId) {
-        this.#replaceFormToPoint(this.#currentEditingPointId);
-      }
-    }
-  };
-
   init() {
     this.#renderBoard();
+  }
+
+  destroy() {
+    this.#clearPoints();
+    this.#clearComponents();
   }
 }
